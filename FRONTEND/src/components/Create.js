c8449465f2d9070b29d5b23f7c7eadbb194ddc23
phanideva <phanisaisri@gmail.com>
2025-01-20 21:43:25 -0600

    created react js frontend; page navigation with react router

---
+++ FRONTEND/src/components/Create.js
@@ -0,0 +1,10 @@
+
+import React from 'react';
+
+const Create = () => {
+    return (
+        <div>create</div>
+    )
+}
+
+export default Create
---
+++ FRONTEND/src/components/Home.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Welcome to the Home Page</h1>
+            <p>This is the default home page for the Django React app.</p>
+        </div>
+    )
+}
+
+export default Home
